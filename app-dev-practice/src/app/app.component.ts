a782cfe8c3b1749df60689e313ea3e4042c4a65c
Piers Kirk <pfk3jd@virginia.edu>
2023-02-02 23:33:45 -0500

    Added functionality to add and delete buttons
    Now if the user clicks the add button a row is added to the html table. The values in the row are hard coded. If the user clicks the delete a button, a row is deleted from the table. Next, need to look into how to format the new rows. Then look at HTML entities.

--- app-dev-practice/src/app/app.component.ts
+++ app-dev-practice/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app-dev-practice';
+  title = 'TV Shows';
+
+  public addShow(tableId: string) {
+    let tableRef = (<HTMLTableElement >document.getElementById(tableId));
+    if (tableRef !== null) {
+      let newRow = tableRef.insertRow(-1);
+      let showNameCell = newRow.insertCell(0);
+      const showName = document.createTextNode('The Last of Us');
+      showNameCell.appendChild(showName);
+      let streamingPlatformCell = newRow.insertCell(1);
+      const streamingPlatform = document.createTextNode('HBO');
+      streamingPlatformCell.appendChild(streamingPlatform);
+      let genreCell = newRow.insertCell(2);
+      const genre = document.createTextNode('Drama');
+      genreCell.appendChild(genre);
+    }
+  }
+
+  public deleteShow(tableId: string) {
+    let tableRef = (<HTMLTableElement >document.getElementById(tableId));
+    if (tableRef !== null) {
+      tableRef.deleteRow(-1);
+    }
+  }
+
 }
