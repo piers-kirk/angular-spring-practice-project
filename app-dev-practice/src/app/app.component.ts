1d589d859877e992ed67d20e29cd638fda4dab62
Piers Kirk <pfk3jd@virginia.edu>
2023-02-03 20:08:14 -0500

    improved functionality of delete button
    Formatted code using prettier. Moving forward, whenever saving the code will be automatically formatted. Made some minor styling changes; now when user hovers over a table row, the row is highlighted in light green. Finally, user can no longer delete the table header. Still need to figure out how to copy the styling so it is the same for new rows.

--- app-dev-practice/src/app/app.component.ts
+++ app-dev-practice/src/app/app.component.ts
@@ -3,21 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'TV Shows';
 
   public addShow(tableId: string) {
-    let tableRef = (<HTMLTableElement >document.getElementById(tableId));
+    let tableRef = <HTMLTableElement>document.getElementById(tableId);
     if (tableRef !== null) {
-      let newRow = tableRef.insertRow(-1);
+      const newRow = tableRef.insertRow(-1);
+
       let showNameCell = newRow.insertCell(0);
       const showName = document.createTextNode('The Last of Us');
       showNameCell.appendChild(showName);
+
       let streamingPlatformCell = newRow.insertCell(1);
       const streamingPlatform = document.createTextNode('HBO');
       streamingPlatformCell.appendChild(streamingPlatform);
+
       let genreCell = newRow.insertCell(2);
       const genre = document.createTextNode('Drama');
       genreCell.appendChild(genre);
@@ -25,10 +28,9 @@ export class AppComponent {
   }
 
   public deleteShow(tableId: string) {
-    let tableRef = (<HTMLTableElement >document.getElementById(tableId));
-    if (tableRef !== null) {
+    let tableRef = <HTMLTableElement>document.getElementById(tableId);
+    if (tableRef !== null && tableRef.childNodes.length != 1) {
       tableRef.deleteRow(-1);
     }
   }
-
 }
