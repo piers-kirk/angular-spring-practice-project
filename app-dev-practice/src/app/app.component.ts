19640cd2217248449dec7cd74c5fb56e1c26864f
Piers Kirk <pfk3jd@virginia.edu>
2023-02-11 20:14:56 -0500

    Updated form
    Updated form so custom arguments can be used to populate table rows; however, delete is no longer working as expected. Will fix and then look to fix CSS so when new rows are added, It includes borders for each table cell. Then, will look to add form validation.

--- app-dev-practice/src/app/app.component.ts
+++ app-dev-practice/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,27 +11,40 @@ export class AppComponent {
   public title: string = "Piers' Application";
   public pageTitle: string = 'Welcome!';
 
-  constructor(private titleService: Title) {}
+  tvShowForm = this.formBuilder.group({
+    tvShow: '',
+    streamingPlatform: '',
+    genre: '',
+  });
+
+  constructor(private titleService: Title, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.titleService.setTitle(this.title);
   }
 
-  public addShow(tableId: string) {
+  onSubmit(tableId: string): void {
     let tableRef = <HTMLTableElement>document.getElementById(tableId);
-    if (tableRef !== null) {
+    if (
+      tableRef !== null &&
+      this.tvShowForm.value.tvShow != null &&
+      this.tvShowForm.value.streamingPlatform != null &&
+      this.tvShowForm.value.genre != null
+    ) {
       const newRow = tableRef.insertRow(-1);
 
       let showNameCell = newRow.insertCell(0);
-      const showName = document.createTextNode('The Last of Us');
+      const showName = document.createTextNode(this.tvShowForm.value.tvShow);
       showNameCell.appendChild(showName);
 
       let streamingPlatformCell = newRow.insertCell(1);
-      const streamingPlatform = document.createTextNode('HBO');
+      const streamingPlatform = document.createTextNode(
+        this.tvShowForm.value.streamingPlatform
+      );
       streamingPlatformCell.appendChild(streamingPlatform);
 
       let genreCell = newRow.insertCell(2);
-      const genre = document.createTextNode('Drama');
+      const genre = document.createTextNode(this.tvShowForm.value.genre);
       genreCell.appendChild(genre);
     }
   }
