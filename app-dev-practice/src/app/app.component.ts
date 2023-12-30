54e1ee99ef574b05efce8679462419f5b97717d0
Piers Kirk <pfk3jd@virginia.edu>
2023-12-30 03:15:41 -0500

    Updating application styling
    Fix Date, move parralax to another component, simplify sass

--- app-dev-practice/src/app/app.component.ts
+++ app-dev-practice/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,60 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent {
   public title: string = "Piers' Application";
-  constructor(private titleService: Title) {}
+  lastUpdateDate: string;
+
+  constructor(
+    private titleService: Title,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
 
   ngOnInit() {
     this.titleService.setTitle(this.title);
+
+    const parallaxSections =
+      this.el.nativeElement.querySelectorAll('.parallax-section');
+    const lastSection = parallaxSections[parallaxSections.length - 1];
+
+    this.renderer.listen('window', 'scroll', () => {
+      const scrollPosition = window.scrollY;
+      const lastSectionBottom =
+        lastSection.offsetTop + lastSection.offsetHeight;
+
+      // If trying to scroll past the last section, set scroll position to the maximum allowed
+      if (scrollPosition > lastSectionBottom - window.innerHeight) {
+        window.scrollTo(0, lastSectionBottom - window.innerHeight);
+      }
+    });
+
+    // Format the date with date-fns-tz
+    this.updateLastUpdateDate();
+  }
+
+  updateLastUpdateDate() {
+    const currentDate = new Date();
+    this.lastUpdateDate = currentDate.toDateString();
+  }
+
+  selectedButtonId = 'home';
+
+  navigateToHome() {
+    this.selectedButtonId = 'home';
+    // Add logic to navigate to the 'Home' page if needed
+  }
+
+  navigateToAbout() {
+    this.selectedButtonId = 'about';
+    // Add logic to navigate to the 'About' page if needed
+  }
+
+  navigateToPortfolio() {
+    this.selectedButtonId = 'portfolio';
+    // Add logic to navigate to the 'Portfolio' page if needed
+  }
+
+  navigateToContact() {
+    this.selectedButtonId = 'contact';
+    // Add logic to navigate to the 'Contact' page if needed
   }
 }
